5ca5ea9f9b9b77447303720c4866e9d9b221f416
nafan <nafanpzk@mail.ru>
2016-04-18 15:06:29 +0300

    Saving data to barbershop.db true-way

--- app.rb
+++ app.rb
@@ -24,17 +24,27 @@ get '/visit' do
 end
 
 post '/visit' do
-	@username = params[:username]
-	@phone = params[:phone]
-	@date_time = params[:date_time]
-	@barber = params[:barber]
 
-	c = Client.new
-	c.name = @username
-	c.phone = @phone
-	c.datestamp = @date_time
-	c.barber = @barber
+# => # СОХРАНЕНИЕ В БАЗУ true-vay
+
+	c = Client.new params[:client]
 	c.save
+
+# => # СОХРАНЕНИЕ В БАЗУ lame-vay
+
+#	@username = params[:username]
+#	@phone = params[:phone]
+#	@date_time = params[:date_time]
+#	@barber = params[:barber]
+#
+#
+#	# Сохраняем значения в таблицу clients
+#	c = Client.new
+#	c.name = @username
+#	c.phone = @phone
+#	c.datestamp = @date_time
+#	c.barber = @barber
+#	c.save
 
 	# хеш ошибок
 	hh = { :username => 'Введите имя', :phone => 'Введите телефон', :date_time => 'Введите дату и время'}
@@ -44,33 +54,4 @@ post '/visit' do
 		return erb :visit
 	end
 
-	#db = get_db
-	#db.execute 'INSERT INTO users (username, phone, datestamp, barber) VALUES ( ?, ?, ?, ?)', [@username, @phone, @date_time, @barber]
-
-	#db.execute "INSERT INTO users (name, phone, datestamp, barber) VALUES (#{@username}, #{@phone}, #{@date_time}, #{@barber});"
-	#db.close
-
-# ИЛИ
-
-#	# для каждой пары ключ-значение
-#	hh.each do |key, value|
-#
-#		# если параметр пуст
-#		if params[key] == ''
-#
-#			# переменной error присвоить значение value из хэша ошибок
-#			@error = hh[key]
-#
-#			return erb ;
-#		end
-#	end
-
-#	f = File.open("/users.txt", "a")
-#	f.write("Name: #{@username.capitalize}, phone: #{@phone}, Date & Time: #{@date_time}, barber: #{@barber}")
-#	f.close
-
-	#@title = "Спасибо!"
-	#@message = "Мы будем ждать вас в #{@date_time}"
-
-	#erb :message
 end
